1fa21d7a98c55e03a81883aa9ed1c1b91e34b992
Huangfei <huangfei_07@163.com>
2016-01-27 19:12:32 +0800

    update

--- Note_Wechat/NSString+MD5.m
+++ Note_Wechat/NSString+MD5.m
@@ -16,7 +16,7 @@
 {
     const char *cStr = [srcString UTF8String];
     unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(cStr, strlen(cStr), digest );
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), digest);
     NSMutableString *result = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
         [result appendFormat:@"%02x", digest[i]];
